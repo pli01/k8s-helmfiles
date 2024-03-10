19b134b144457f6afccb641970846f5685a39620
pli <pli01@users.noreply.github.com>
2024-03-10 21:25:19 +0100

    fix (#6)

--- ci/tests/test-local-url.sh
+++ ci/tests/test-local-url.sh
@@ -5,92 +5,33 @@ set -e -o pipefail
 echo "Test local urls"
 curl_args="--connect-timeout 30 --retry 300 --retry-delay 5"
 
-timeout=180
-test_result=1
-echo "# https://prometheus.127.0.0.1.nip.io"
-until [ "$timeout" -le 0 -o "$test_result" -eq "0" ] ; do
-  ( curl -Lsv https://prometheus.127.0.0.1.nip.io  2>&1 |grep "<title>Prometheus Time Series Collection and Processing Server</title>" )
-  test_result=$?
-  if [ "$test_result" -gt 0 ] ;then
-     echo "Retry $timeout seconds: $test_result";
-     (( timeout-- ))
-     sleep 1
-  fi
-done
-if [ "$test_result" -gt 0 ] ;then
-        test_status=ERROR
-        echo "$test_status: curl result: $test_result"
-        exit $test_result
-fi
+function test_url() {
+    url="$1"
+    message="$2"
+    timeout="$3"
+    interval="$4"
+    test_result=1
+    until [ "$timeout" -le 0 -o "$test_result" -eq "0" ] ; do
+      ( curl $curl_args -Lsv "$url" 2>&1 |grep "$message" )
+      test_result=$?
+      if [ "$test_result" -gt 0 ] ;then
+         echo "URL $url is not accessible yet. Retrying in $timeout seconds: $test_result";
+         (( timeout-- ))
+         sleep $interval
+      fi
+    done
+    if [ "$test_result" -gt 0 ] ;then
+            test_status=ERROR
+            echo "$test_status: Timeout reached. URL $url is not accessible. result: $test_result"
+            return $test_result
+    fi
+    echo "URL $url is accessible."
+}
 
 timeout=180
-test_result=1
-echo "# https://alertmanager.127.0.0.1.nip.io"
-until [ "$timeout" -le 0 -o "$test_result" -eq "0" ] ; do
-  ( curl -Lsv https://alertmanager.127.0.0.1.nip.io 2>&1  |grep "<title>Alertmanager</title>" )
-  test_result=$?
-  if [ "$test_result" -gt 0 ] ;then
-     echo "Retry $timeout seconds: $test_result";
-     (( timeout-- ))
-     sleep 1
-  fi
-done
-if [ "$test_result" -gt 0 ] ;then
-        test_status=ERROR
-        echo "$test_status: curl result: $test_result"
-        exit $test_result
-fi
 
-timeout=180
-test_result=1
-echo "# https://grafana.127.0.0.1.nip.io"
-until [ "$timeout" -le 0 -o "$test_result" -eq "0" ] ; do
-  ( curl -Lsv https://grafana.127.0.0.1.nip.io 2>&1 |grep "<title>Grafana</title>" )
-  test_result=$?
-  if [ "$test_result" -gt 0 ] ;then
-     echo "Retry $timeout seconds: $test_result";
-     (( timeout-- ))
-     sleep 1
-  fi
-done
-if [ "$test_result" -gt 0 ] ;then
-        test_status=ERROR
-        echo "$test_status: curl result: $test_result"
-        exit $test_result
-fi
-
-timeout=180
-test_result=1
-echo "# https://argocd.127.0.0.1.nip.io"
-until [ "$timeout" -le 0 -o "$test_result" -eq "0" ] ; do
-  ( curl $curl_args  -Lsv https://argocd.127.0.0.1.nip.io  2>&1 |grep "<title>Argo CD</title>" )
-  test_result=$?
-  if [ "$test_result" -gt 0 ] ;then
-     echo "Retry $timeout seconds: $test_result";
-     (( timeout-- ))
-     sleep 1
-  fi
-done
-if [ "$test_result" -gt 0 ] ;then
-        test_status=ERROR
-        echo "$test_status: curl result: $test_result"
-        exit $test_result
-fi
-
-timeout=180
-test_result=1
-echo "# https://whoami.127.0.0.1.nip.io"
-until [ "$timeout" -le 0 -o "$test_result" -eq "0" ] ; do
-  ( curl -Lsv https://whoami.127.0.0.1.nip.io  2>&1 |grep "^Hostname" )
-  test_result=$?
-  if [ "$test_result" -gt 0 ] ;then
-     echo "Retry $timeout seconds: $test_result";
-     (( timeout-- ))
-     sleep 1
-  fi
-done
-if [ "$test_result" -gt 0 ] ;then
-        test_status=ERROR
-        echo "$test_status: curl result: $test_result"
-        exit $test_result
-fi
+test_url "https://alertmanager.127.0.0.1.nip.io" "<title>Alertmanager</title>" $timeout 1 || exit $?
+test_url "https://prometheus.127.0.0.1.nip.io" "<title>Prometheus Time Series Collection and Processing Server</title>" $timeout 1 || exit $?
+test_url "https://grafana.127.0.0.1.nip.io" "<title>Grafana</title>" $timeout 1 || exit $?
+test_url "https://argocd.127.0.0.1.nip.io" "<title>Argo CD</title>" $timeout 1 || exit $?
+test_url "https://whoami.127.0.0.1.nip.io" "^Hostname" $timeout 1 || exit $?
